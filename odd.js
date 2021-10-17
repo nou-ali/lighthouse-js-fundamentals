643d944f06d39dc2b676d8ced3974f03c77f21bc
nou-ali <88689756+nou-ali@users.noreply.github.com>
2021-10-17 14:41:02 -0400

    Delete odd.js

--- odd.js
+++
@@ -1,9 +0,0 @@
-const isOdd = function (num) {
-  if (num % 2 == 0) {
-    return "even";
-  } else {
-    return "odd";
-  }
-}
-console.log("3 is odd: " + isOdd(3));
-console.log("8 is odd: " + isOdd(8));
