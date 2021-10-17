41b6257253e701ef0cccf226b85432103416ed60
nou-ali <noura4403@gmail.com>
2021-10-17 00:07:38 +0000

    Declaring and Calling Functions

---
+++ hello.js
@@ -0,0 +1,13 @@
+const sayHello = function () {
+  console.log("Hello,World!");
+}
+
+sayHello();
+
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("Noura");
+sayHello("Fadumo");
+sayHello("Huda");
