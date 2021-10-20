0b8a42c3b99503261239ee01edac955900862e50
nou-ali <noura4403@gmail.com>
2021-10-20 20:31:54 +0000

    Iterating Over Arrays

--- packing-list.js
+++ packing-list.js
@@ -1,5 +1,14 @@
+// Iterating Over an Array With for loop
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i< packingList.length; i++) {
 console.log(packingList[i]);
+}
+
+// Iterating Over an Array With while loop
+
+console.log("Kitchen stuff to pack:");
+let i = 0
+while (i < packingList.length) {
+  i++;
 }
