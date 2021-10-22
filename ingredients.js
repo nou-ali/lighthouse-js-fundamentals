d44b9659ed91d4064cb50a8c47b796205c681981
Nou <nou@MacBook-Pro.local>
2021-10-22 16:31:43 -0400

    Creating loops with arrays

---
+++ ingredients.js
@@ -0,0 +1,20 @@
+// Write a while loop that prints out the contents of ingredients:
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+const list = 0; // when to start
+while (list.length) { // when to stop
+  list++; // how to get to the next item
+}
+console.log(ingredients);
+
+// Write a for loop that prints out the contents of ingredients:
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = 0; i < ingredients.length; i++) {
+}
+console.log(ingredients);
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+for (var i = 0; i < ingredients.length; i++) {
+}
+ingredients.reverse();
+console.log(ingredients);
