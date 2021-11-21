cee88df7f6e545a47b6229c3e62a4ed6d4c25d54
nou-ali <noura4403@gmail.com>
2021-11-21 18:30:37 +0000

    Initial commit with all my work so far

---
+++ tinkering.js
@@ -0,0 +1,21 @@
+
+
+function multiply(a, b) {
+  return a * b
+}
+
+const result = multiply (2, 4);
+console.log (result);
+
+
+var firstName = Julia;
+var interest = cats;
+var hobby = video games;
+
+
+var firstName2 = Hi, my name is James;
+var interest2 = I love baseball;
+var hobby2 = In my spare time, I like to read;
+
+var awesomeMessage = firstName2 + interest + hobby;
+console.log(awesomeMessage);
